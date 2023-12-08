e4764cce4b50b81e189b052d82ca4f3063a45a47
Arman Augusto <arman.augusto@gmail.com>
2023-12-08 15:30:02 -0800

    added initial futureValue calculation

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Investment Calculator")
+	var investmentAmount int
+	var expectedReturnRate float64
+	var years int
+
+	investmentAmount = 1000
+	expectedReturnRate = 5.5
+	years = 10
+
+	var futureValue = float64(investmentAmount) * math.Pow(1 + expectedReturnRate / 100, float64(years))
+	fmt.Println(futureValue)
 }
